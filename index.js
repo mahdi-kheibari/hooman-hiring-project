13c41420bd223563968663e46092e4ab81b576c2
mahdi-kheibari <mahdi.khaybari@gmail.com>
2023-02-28 00:24:42 +0330

    header was added

---
+++ index.js
@@ -0,0 +1,9 @@
+function setActive(index) {
+    const items = document.getElementsByClassName("header-item")
+    for (i = 0; i < items.length; i++) {
+        if (items[i].className.search("active")!==-1) {
+            items[i].className = items[i].className.replace(" active", "");
+        }
+    }
+    items[index].className += " active";
+}
