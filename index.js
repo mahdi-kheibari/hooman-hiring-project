940578773eeb6d6637a398e69f501895e1c65f37
mahdi-kheibari <mahdi.khaybari@gmail.com>
2023-02-28 18:12:01 +0330

    header was responsived

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// for header
 function setActive(index) {
     const items = document.getElementsByClassName("header-item")
     for (i = 0; i < items.length; i++) {
@@ -6,4 +7,10 @@ function setActive(index) {
         }
     }
     items[index].className += " active";
+}
+function openSidebar() {
+    document.getElementsByClassName("sidebar")[0].style.display="block";
+}
+function closeSidebar() {
+    document.getElementsByClassName("sidebar")[0].style.display="none";
 }
