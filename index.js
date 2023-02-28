43f7891c4d8f2b461c5f7c6f3544179f78ce41c3
mahdi-kheibari <mahdi.khaybari@gmail.com>
2023-02-28 19:28:04 +0330

    slider was added and was responsived

--- index.js
+++ index.js
@@ -14,3 +14,31 @@ function openSidebar() {
 function closeSidebar() {
     document.getElementsByClassName("sidebar")[0].style.display="none";
 }
+
+// for slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slider-Slides");
+  let paginations = document.getElementsByClassName("paginations-item");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < paginations.length; i++) {
+    paginations[i].className = paginations[i].className.replace(" activeSlide", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  paginations[slideIndex-1].className += " activeSlide";
+}
